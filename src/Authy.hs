d75e50303f222e768b15c046473b41312e6308dd
Krzysztof Woś <krzysztof.wos@gmail.com>
2013-11-26 23:32:11 +0900

    Simplify the API by removing choice of response format since it is an implementation detail and improve documentation.

--- src/Authy.hs
+++ src/Authy.hs
@@ -1,8 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Authy
-    ( Format(JSON)
-    , newUser
+    ( newUser
     , verify
     , apiServer
     , sandboxServer
@@ -18,18 +17,11 @@ import           Network.Curl.Opts     (CurlOption(..))
 
 -------------------------------------------------------------------------------
 
-data Format = XML | JSON deriving Eq
-
-instance Show Format where
-    show JSON = "json"
-    show XML  = "xml"
-
-
--------------------------------------------------------------------------------
-
 apiServer, sandboxServer :: String
-apiServer     = "https://api.authy.com"        -- Production API
-sandboxServer = "http://sandbox-api.authy.com" -- Sandbox API (great for automated testing)
+-- | Production API server (<https://api.authy.com>).
+apiServer     = "https://api.authy.com"
+-- | Sandbox API server (<http://sandbox-api.authy.com>). Great for automated testing.
+sandboxServer = "http://sandbox-api.authy.com"
 
 
 -------------------------------------------------------------------------------
@@ -51,31 +43,28 @@ newUserResponseParser = withObject "new user response" $ \obj -> do
     where
         keys = ["email", "cellphone", "country_code"]
 
--- | Enabling two-factor authentication on a user
+-- | Enable two-factor authentication on a user.
 newUser
     :: String
-    -- ^ "https://api.authy.com" or "http://sandbox-api.authy.com"
-    -> Format
-    -- ^ JSON or XML
+    -- ^ API server.
     -> String
-    -- ^ Your private API key
+    -- ^ Private API key.
     -> String
-    -- ^ E-mail
+    -- ^ E-mail address.
     -> String
-    -- ^ Cell phone. Used by the API to match the user
+    -- ^ Cell phone. Used by the API to match the user.
     -> Int
-    -- ^ Numeric calling country code of the country. Eg: 1 for the US. 91 for India. 54 for Mexico
+    -- ^ Numeric calling country code of the country. Eg: 1 for the US. 91 for India. 54 for Mexico.
     -> IO (Either (String, [String]) Int)
-    -- ^ Either an error message or a user ID
-newUser server format key email cellPhone countryCode
-    | format == XML = error "XML requests are not yet implemented"
-    | otherwise     = curlAeson newUserResponseParser "POST" (server ++ path)
-                          [CurlFailOnError False, CurlPostFields formData] noData
+    -- ^ Either an error message or a user ID.
+newUser server key email cellPhone countryCode =
+    curlAeson newUserResponseParser "POST" (server ++ path)
+        [CurlFailOnError False, CurlPostFields formData] noData
     where
         formData = ["user[email]=" ++ email
                    ,"user[cellphone]=" ++ cellPhone
                    ,"user[country_code]=" ++ show countryCode]
-        path     = "/protected/" ++ show format ++ "/users/new?api_key=" ++ key
+        path     = "/protected/json/users/new?api_key=" ++ key
 
 
 -------------------------------------------------------------------------------
@@ -92,24 +81,21 @@ verifyResponseParser = withObject "VerifyResponse" $ \obj -> do
                 _                  -> return $ Left message
         _       -> return $ Left $ "The 'impossible' happened: Unknown value for key 'success': '" ++ success ++ "'"
 
--- | Verifying a user token
+-- | Verify a user token.
 verify
     :: String
-    -- ^ "https://api.authy.com" or "http://sandbox-api.authy.com"
-    -> Format
-    -- ^ JSON or XML
+    -- ^ API server.
     -> String
-    -- ^ Your private API key
+    -- ^ Private API key.
     -> String
-    -- ^ The token you are verifying
+    -- ^ The token you are verifying.
     -> String
-    -- ^ The authy ID that was sent back when registering the users device
+    -- ^ The Authy ID that was sent back when registering the users device.
     -> Bool
-    -- ^ Force token verification on unregistered user
+    -- ^ Force token verification on unregistered user.
     -> IO (Either String Bool)
-    -- ^ Either an error message or a Boolean indicating whether the token is valid
-verify server format key token authyID force
-    | format == XML = error "XML requests are not yet implemented"
-    | otherwise     = curlAeson verifyResponseParser "GET" url [CurlFailOnError False] noData
+    -- ^ Either an error message or a Boolean indicating whether the token is valid.
+verify server key token authyID force =
+    curlAeson verifyResponseParser "GET" url [CurlFailOnError False] noData
     where
-        url = server ++ "/protected/" ++ show format ++ "/verify/" ++ token ++ "/" ++ authyID ++ "?api_key=" ++ key ++ if force then "&force=true" else ""
+        url = server ++ "/protected/json/verify/" ++ token ++ "/" ++ authyID ++ "?api_key=" ++ key ++ if force then "&force=true" else ""
--- tests/AuthySpec.hs
+++ tests/AuthySpec.hs
@@ -4,7 +4,7 @@ module AuthySpec where
 
 import Test.Hspec (Spec, context, describe, hspec, it, shouldReturn)
 
-import Authy      (Format(..), newUser, sandboxServer, verify)
+import Authy      (newUser, sandboxServer, verify)
 
 spec :: Spec
 spec = do
@@ -17,7 +17,7 @@ spec = do
            -d user[country_code]="54" -}
         context "Valid new user request" $
             it "returns a user ID" $
-                newUser sandboxServer JSON key "user@domain.com" "317-338-9302" 54 `shouldReturn` Right 209
+                newUser sandboxServer key "user@domain.com" "317-338-9302" 54 `shouldReturn` Right 209
 
         {- curl http://sandbox-api.authy.com/protected/json/users/new?api_key=d57d919d11e6b221c9bf6f7c882028f9 \
            -d user[email]="user.com" \
@@ -25,7 +25,7 @@ spec = do
            -d user[country_code]="1" -}
         context "Request with an invalid e-mail" $
             it "returns an error message" $
-                newUser sandboxServer JSON key "user.com" "AAA-338-9302" 1 `shouldReturn` Left ("User was not valid", ["email"])
+                newUser sandboxServer key "user.com" "AAA-338-9302" 1 `shouldReturn` Left ("User was not valid", ["email"])
 
         {- curl http://sandbox-api.authy.com/protected/json/users/new?api_key=d57d919d11e6b221c9bf6f7c882028f9 \
            -d user[email]="user.com" \
@@ -33,7 +33,7 @@ spec = do
            -d user[country_code]="999" -}
         context "Request with errors an invalid e-mail and country code" $
             it "returns an error message" $
-                newUser sandboxServer JSON key "user.com" "AAA-338-9302" 999 `shouldReturn` Left ("User was not valid", ["email", "country_code"])
+                newUser sandboxServer key "user.com" "AAA-338-9302" 999 `shouldReturn` Left ("User was not valid", ["email", "country_code"])
 
         {- curl http://sandbox-api.authy.com/protected/json/users/new?api_key=d57d919d11e6b221c9bf6f7c882028f9 \
            -d user[email]="user.com" \
@@ -41,28 +41,28 @@ spec = do
            -d user[country_code]="999" -}
         context "Request with an invalid e-mail and cell phone" $
             it "returns an error message" $
-                newUser sandboxServer JSON key "user.com" "" 1 `shouldReturn` Left ("User was not valid", ["email", "cellphone"])
+                newUser sandboxServer key "user.com" "" 1 `shouldReturn` Left ("User was not valid", ["email", "cellphone"])
 
     describe "verify" $ do
         -- curl -i http://sandbox-api.authy.com/protected/json/verify/0000000/1?api_key=d57d919d11e6b221c9bf6f7c882028f9
         context "Verification of a valid token" $
             it "returns True" $
-                verify sandboxServer JSON key "0000000" "1" False `shouldReturn` Right True
+                verify sandboxServer key "0000000" "1" False `shouldReturn` Right True
 
         -- curl -i http://sandbox-api.authy.com/protected/json/verify/1234567/1\?api_key\=d57d919d11e6b221c9bf6f7c882028f9
         context "Verification of a non-existent user" $
             it "returns an error message" $
-                verify sandboxServer JSON key "1234567" "1" False `shouldReturn` Left "User doesn't exist."
+                verify sandboxServer key "1234567" "1" False `shouldReturn` Left "User doesn't exist."
 
         -- curl -i http://sandbox-api.authy.com/protected/json/verify/939393/3?api_key=d57d919d11e6b221c9bf6f7c882028f9
         context "When a user has not finished registration" $ 
             it "returns False" $
-                verify sandboxServer JSON key "939393" "3" False `shouldReturn` Right False
+                verify sandboxServer key "939393" "3" False `shouldReturn` Right False
 
         -- curl -i http://sandbox-api.authy.com/protected/json/verify/939393/3?api_key=d57d919d11e6b221c9bf6f7c882028f9\&force=true
         context "Forced verification on user who has not finished registration" $
             it "returns False" $
-                verify sandboxServer JSON key "939393" "3" True `shouldReturn` Right False
+                verify sandboxServer key "939393" "3" True `shouldReturn` Right False
 
 main :: IO ()
 main = hspec spec
