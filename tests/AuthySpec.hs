6173e6897c22e295895d6e8c72a25259096e68fe
Krzysztof Woś <krzysztof.wos@gmail.com>
2014-06-25 23:21:06 +0900

    Fix tests.

--- tests/AuthySpec.hs
+++ tests/AuthySpec.hs
@@ -25,7 +25,7 @@ spec = do
            -d user[country_code]="1" -}
         context "Request with an invalid e-mail" $
             it "returns an error message" $
-                newUser sandboxServer key "user.com" "AAA-338-9302" 1 `shouldReturn` Left ("User was not valid", ["email"])
+                newUser sandboxServer key "user.com" "AAA-338-9302" 1 `shouldReturn` Left ("User was not valid.", ["email"])
 
         {- curl http://sandbox-api.authy.com/protected/json/users/new?api_key=d57d919d11e6b221c9bf6f7c882028f9 \
            -d user[email]="user.com" \
@@ -33,7 +33,7 @@ spec = do
            -d user[country_code]="999" -}
         context "Request with errors an invalid e-mail and country code" $
             it "returns an error message" $
-                newUser sandboxServer key "user.com" "AAA-338-9302" 999 `shouldReturn` Left ("User was not valid", ["email", "country_code"])
+                newUser sandboxServer key "user.com" "AAA-338-9302" 999 `shouldReturn` Left ("User was not valid.", ["email", "country_code"])
 
         {- curl http://sandbox-api.authy.com/protected/json/users/new?api_key=d57d919d11e6b221c9bf6f7c882028f9 \
            -d user[email]="user.com" \
@@ -41,28 +41,33 @@ spec = do
            -d user[country_code]="999" -}
         context "Request with an invalid e-mail and cell phone" $
             it "returns an error message" $
-                newUser sandboxServer key "user.com" "" 1 `shouldReturn` Left ("User was not valid", ["email", "cellphone"])
+                newUser sandboxServer key "user.com" "" 1 `shouldReturn` Left ("User was not valid.", ["email", "cellphone"])
 
     describe "verify" $ do
         -- curl -i http://sandbox-api.authy.com/protected/json/verify/0000000/1?api_key=d57d919d11e6b221c9bf6f7c882028f9
-        context "Verification of a valid token" $
+        context "verification of a valid token" $
             it "returns True" $
                 verify sandboxServer key "0000000" "1" False `shouldReturn` Right True
 
         -- curl -i http://sandbox-api.authy.com/protected/json/verify/1234567/1\?api_key\=d57d919d11e6b221c9bf6f7c882028f9
-        context "Verification of a non-existent user" $
+        context "verification of a non-existent user" $
             it "returns an error message" $
                 verify sandboxServer key "1234567" "1" False `shouldReturn` Left "User doesn't exist."
 
         -- curl -i http://sandbox-api.authy.com/protected/json/verify/939393/3?api_key=d57d919d11e6b221c9bf6f7c882028f9
-        context "When a user has not finished registration" $ 
-            it "returns False" $
-                verify sandboxServer key "939393" "3" False `shouldReturn` Right False
+        context "when a user has not finished registration" $ 
+            it "returns an error message" $
+                verify sandboxServer key "939393" "3" False `shouldReturn` Left "User doesn't exist."
 
         -- curl -i http://sandbox-api.authy.com/protected/json/verify/939393/3?api_key=d57d919d11e6b221c9bf6f7c882028f9\&force=true
-        context "Forced verification on user who has not finished registration" $
-            it "returns False" $
-                verify sandboxServer key "939393" "3" True `shouldReturn` Right False
+        context "forced verification on user who has not finished registration" $
+            it "returns an error message" $
+                verify sandboxServer key "939393" "3" True `shouldReturn` Left "User doesn't exist."
+
+        -- curl -i http://sandbox-api.authy.com/protected/json/verify/939393/3?api_key=d57d919d11e6b221c9bf6f7c882028f8
+        context "invalid API key" $
+            it "returns an error message" $
+                verify sandboxServer "0123456789abcdef0123456789abcdef" "939393" "3" True `shouldReturn` Left "Invalid API key."
 
 main :: IO ()
 main = hspec spec
